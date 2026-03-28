eedd905917783eb1987777ba66f7d538ed690cd0
chemfac <chemfac000@gmail.com>
2026-03-28 21:41:05 +0900

    feat: add MathJax support for LaTeX rendering and create new question pages for 2024 (q31-q34)

--- script.js
+++ script.js
@@ -113,3 +113,22 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+// Load MathJax for rendering LaTeX equations
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    processEscapes: true
+  },
+  options: {
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre']
+  }
+};
+
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
